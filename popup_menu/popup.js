747ac9e0f56654a8003a9bdd96ff8e10cf41bd3a
Akych <akychrak@gmail.com>
2020-05-25 21:58:00 +0500

    upload v_1

---
+++ popup_menu/popup.js
@@ -0,0 +1,128 @@
+
+$(()=>{
+
+    const randKey = (i) => {      
+        var rnd = ''
+        while (rnd.length < i) 
+            rnd += Math.random().toString(36).substring(2)
+        return rnd.substring(0, i)
+    }
+    var switcher = false
+
+    const names = [
+        "Зашифруй свои сообщения",
+        "Vk Message Cryptor",
+        "Мы против секретов",
+        "Понтанись перед друзьями"
+    ]
+    $("#title_msg").text( names[ Math.floor(Math.random() * names.length)] )
+
+    chrome.storage.local.get('settings', function(data) {
+        let arr = data["settings"] 
+        $("#key_send_text").val( arr.key  || "LibGear_Akych" )  
+        $("#pod_send_text").val( arr.code || "LibGear_team" )
+        switcher = arr.enter_send || false
+        setSwitch(switcher);
+    });
+ 
+
+    const save_all = ()=>{
+        chrome.storage.local.set({
+            "settings" :{
+                "key" : $("#key_send_text").val(),
+                "code": $("#pod_send_text").val(),
+                "enter_send": switcher
+            }
+        });
+    }
+
+
+    const contents = chrome.extension.getURL("i");
+
+    const setHover = (btn,def,hover,click)=>{
+       let image = btn.children().first()
+        btn.mouseleave(()=>{
+            image.attr("src",def);
+        })
+        btn.mouseenter(()=>{
+            image.attr("src",hover);
+        })
+        btn.mousedown(()=>{
+            image.attr("src",click);
+        })        
+        btn.mouseup(()=>{
+            image.attr("src",hover);
+        })
+    }
+
+    const key_send = $("#key_send")
+    setHover(key_send,contents+"/images/Accept-default.png" , contents+"/images/Accept-hover.png",contents+"/images/Accept-click.png")
+    key_send.click(save_all)
+
+    const pod_send = $("#pod_send")
+    setHover(pod_send,contents+"/images/Accept-default.png" , contents+"/images/Accept-hover.png",contents+"/images/Accept-click.png")
+    $("#pod_send").click(save_all)
+
+    const key_rand = $("#key_rand")
+    setHover(key_rand,contents+"/images/Random-default.png" , contents+"/images/Random-hover.png",contents+"/images/Random-click.png")
+    key_rand.click(()=>{
+        $("#key_send_text").val( randKey(16) )
+    })
+/*
+    const pod_rand = $("#pod_rand")
+    setHover(pod_rand,contents+"/images/Random-default.png" , contents+"/images/Random-hover.png",contents+"/images/Random-click.png")
+    pod_rand .click(()=>{
+        $("#pod_send_text").val( randKey(16) )
+    })
+*/
+
+    
+    var switcher_value = "";
+    const setSwitch = (switcher) => {
+      if (switcher) {
+        $(".point").css("margin-left", "20px");
+        $(".centr").css("border-radius", "5px 0px 0px 5px");
+        $(".var-2").css("border-radius", "0px 5px 5px 0px");
+        $(".var-2").css("background-color", "#fff");
+        $(".var-1").css("background-color", "#bfcee0");
+        switcher_value = $(".var-2").text();
+      } else {
+        $(".point").css("margin-left", "-3px");
+        $(".centr").css("border-radius", "0px 5px 5px 0px");
+        $(".var-1").css("border-radius", "5px 0px 0px 5px");
+        $(".var-1").css("background-color", "#fff");
+        $(".var-2").css("background-color", "#bfcee0");
+        switcher_value = $(".var-1").text();
+      }
+      save_all()
+      $("#output").text(switcher_value);
+    };
+    
+    switcher_value = $(".var-1").text();
+    $("#output").text($(".var-1").text());
+    $(".selector").click(function () {
+      switcher = !switcher;
+      setSwitch(switcher);
+    });
+    $(".var-1").click(() => {
+      switcher = false;
+      setSwitch(switcher);
+    });
+    $(".var-2").click(() => {
+      switcher = true;
+      setSwitch(switcher);
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+})
+
---
+++ scripts/index.js
@@ -0,0 +1,125 @@
+$(()=>{
+    var chatBox_main =  $('._im_chat_input_parent')
+    if (!chatBox_main) return
+    var key = "говноключ"
+    var signature = "123"
+    var onCEnter = false
+    const spec_symbol = `⁣`
+    const sailt = `U2FsdGVkX1`
+    chrome.storage.local.get('settings', function(data) {
+        
+        key = data["settings"].key
+        signature = data["settings"].code
+        onCEnter = data["settings"].enter_send
+
+        if(onCEnter){
+            // О ВЕЛИКАЙ КАСТЫЛЬ НАВАЛИЛИ, О ШИКАРНЫЙ КОСТЫЛЬ НАВАЛИЛИ
+            var Contrl = false
+            chatBox_textenter.keydown(function (e) {
+                if(e.keyCode == 17){
+                    Contrl = true
+                }
+            })
+            chatBox_textenter.keyup(function (e) {
+                if(Contrl){
+                    if (e.keyCode == 13){
+                        crypto_button.click()
+                    }
+                    if(e.keyCode == 17){
+                        Contrl = false
+                    }
+                }
+            });
+        }
+    });
+
+    chrome.storage.onChanged.addListener(function(changes, namespace) {
+          var storageChange = changes["settings"];
+          if(storageChange){
+            key = storageChange.key
+            signature = storageChange.code
+            onCEnter = storageChange.enter_send
+            location.reload(); 
+          }
+    });
+
+    function randKey(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+    const contents = chrome.extension.getURL("i");
+    console.log(contents)
+
+
+    var chatBox_buttonPanel = $('.im_chat-input--buttons')
+    var chatBox_textenter = $('.im-chat-input--text')
+        chatBox_textenter.css('background','RGB(250, 250, 250)');
+
+    var crypto_button = $("<button><img id='LibGear_crypto_button' src='"+ contents+"/images/2.png'"+" height='21px' ></button>")
+        crypto_button.css('background','RGBA(0, 0, 0, 0)');
+        crypto_button.css('border','0px solid');
+        crypto_button.css('margin-left','-15px');
+        crypto_button.css('margin-top','2px');
+        crypto_button.css('cursor','pointer');
+
+    
+    crypto_button.mouseleave(()=>{
+        Icon.attr("src",contents+"/images/2.png");
+    })
+
+    crypto_button.mouseenter(()=>{
+        Icon.attr("src",contents+"/images/2_hover.png");
+    })
+
+    setInterval(() => {
+        $("._im_log_body").each(function(){
+            let msg =  $( this )
+            if( !msg.attr('class').endsWith('detected')){
+             try {
+
+                var decryptedBytes = CryptoJS.AES.decrypt(msg.text(), key);
+                var plaintext = decryptedBytes.toString(CryptoJS.enc.Utf8);
+
+                let r_key = plaintext.substring(0, 30);
+
+                let k_msg = sailt+plaintext.substring(31, plaintext.length);
+
+                console.log(r_key,k_msg)
+                
+                var decryptedBytes = CryptoJS.AES.decrypt(k_msg, r_key);
+                plaintext = decryptedBytes.toString(CryptoJS.enc.Utf8);
+                let array = JSON.parse(plaintext)
+               // if (array.signature){
+                    msg.text( array.msg )
+                    msg.prepend($(`<span style='color:RGB(100,100,100)' >${array.signature} </span><span style='color:RGB(160,160,160)' >➤ </span>`))
+               // }
+              } catch (err) {}
+                msg.addClass('detected');
+            }
+        })
+    }, 1500);
+
+    chatBox_buttonPanel.append(crypto_button)
+    var Icon = $('#LibGear_crypto_button')
+    var sendButton = $(".im-chat-input--send")
+
+    crypto_button.click(()=>{
+
+        var text = chatBox_textenter.text()
+        if (text.length < 0 || text == ""){
+            return 
+        }
+
+        let r_key = sailt + randKey(20)
+        console.log("r_key len - ",r_key.length)
+        let json = `{"signature" : "${signature}" , "msg" : "${text}"}`
+        let msg =  CryptoJS.AES.encrypt(r_key+spec_symbol+ CryptoJS.AES.encrypt(json, r_key).toString().replace(/^(U2FsdGVkX1)/g,"") ,key) 
+        chatBox_textenter.text( msg )
+        sendButton.click()
+    })
+})
